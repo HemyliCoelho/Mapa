b1a089c0ed426cc1cb2ffae005c3eb8a801da0d4
Hemyli <hemylicoelhoneto@gmail.com>
2024-02-18 20:12:59 -0300

    Navegação tab home/mapa

--- App.js
+++ App.js
@@ -1,28 +1,26 @@
 import * as React from "react";
-import { Button, View } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Mapa from "./Mapa";
-import Pastelaria from "./Pastelaria";
+import Home from "./Home";
 
-
-function PastelariaScreen({ navigation }) {
-  return <Pastelaria></Pastelaria>;
-}
+function HomeScreen({navigation}) {
+  return <Home></Home>
+ }
 
 function MapaScreen({ navigation }) {
   return <Mapa></Mapa>;
 }
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Pastelaria">
-        <Drawer.Screen name="Pastelaria" component={PastelariaScreen} />
-        <Drawer.Screen name="Mapa" component={MapaScreen} />
-      </Drawer.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Mapa" component={MapaScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
